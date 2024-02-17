afd94bcc083101501fcbe1b2051d98aeae45d4f7
gman85 <jimaniya@gmail.com>
2024-02-17 20:57:33 +0900

    push 테스트

---
+++ src/study01/HelloWorld.java
@@ -0,0 +1,14 @@
+package study01;
+
+/**
+ * 테스트 메인
+ * @author gman
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// study01 테스트
+	}
+
+}
